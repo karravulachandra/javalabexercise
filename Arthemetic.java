fba7848fa5d664a0cce31ba36f78d09353938a53
karravula chandra <94832445+karravulachandra@users.noreply.github.com>
2024-06-03 16:10:05 +0530

    Arthemetic Program

---
+++ Arthemetic.java
@@ -0,0 +1,30 @@
+package day1;
+
+public class Arthemetic {
+	    public static void main(String[] args) {
+	        // Addition
+	        int sum = 10 + 5; // sum is 15
+	        
+	        // Subtraction
+	        int difference = 10 - 5; // difference is 5
+	        
+	        // Multiplication
+	        int product = 10 * 5; // product is 50
+	        
+	        // Division
+	        int quotient1 = 10 / 3; // quotient1 is 3 (integer division, remainder discarded)
+	        double quotient2 = 10.0 / 3; // quotient2 is 3.3333333333333335 (floating-point division)
+	        
+	        // Modulus
+	        int remainder = 10 % 3; // remainder is 1 (remainder of 10 divided by 3 is 1)
+	               
+	        // Displaying results
+	        System.out.println("Sum: " + sum);
+	        System.out.println("Difference: " + difference);
+	        System.out.println("Product: " + product);
+	        System.out.println("Quotient1: " + quotient1);
+	        System.out.println("Quotient2: " + quotient2);
+	        System.out.println("Remainder: " + remainder);
+	    }
+	}
+}
